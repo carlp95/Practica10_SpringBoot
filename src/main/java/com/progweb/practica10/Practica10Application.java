b798357c62ac60a2b140ec54e661fc731b247b6e
ndanield <ndanield@hotmail.com>
2018-10-03 11:16:13 -0400

    Bootstrap de server h2 agregado

--- src/main/java/com/progweb/practica10/Practica10Application.java
+++ src/main/java/com/progweb/practica10/Practica10Application.java
@@ -1,14 +1,23 @@
 package com.progweb.practica10;
 
 import com.progweb.practica10.services.SecurityServices;
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Practica10Application {
-
     public static void main(String[] args) {
+        try {
+            Server.createTcpServer(
+                    "-tcpAllowOthers",
+                    "-tcpDaemon").start();
+        }catch (SQLException ex){
+            System.out.println("Problema con la base de datos: "+ex.getMessage());
+        }
         ApplicationContext applicationContext = SpringApplication.run(Practica10Application.class, args);
 
         SecurityServices securityServices = (SecurityServices) applicationContext.getBean("securityServices");
